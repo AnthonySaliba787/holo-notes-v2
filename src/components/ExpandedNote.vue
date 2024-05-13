8de6a99ae07344803e95f26b99bb3db8649d7663
Anthony Saliba <anthonysaliba93@gmail.com>
2024-05-13 17:33:37 +0300

    Implement note design

--- src/components/ExpandedNote.vue
+++ src/components/ExpandedNote.vue
@@ -1,1 +1,27 @@
-<template></template>
+<template>
+  <div v-for="note in notes" :key="note.id">
+    <div v-if="note.expandNote" class="">
+      <div class="">
+        <div class="">
+          <h2>{{ note.title }}</h2>
+          <p>{{ note.desc }}</p>
+        </div>
+        <div class="">
+          <button class="" @click="deleteNote(note.id)">Delete</button>
+          <button @click="note.expandNote = false">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { inject } from "vue";
+
+const notes = inject("notes");
+const emit = defineEmits(["deleteNote"]);
+
+function deleteNote(noteID) {
+  emit("deleteNote", noteID);
+}
+</script>
--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,7 +1,22 @@
 <template>
   <div
-    class="w-full flex-1 flex flex-col justify-center items-center text-center my-8"
+    class="w-full flex-1 flex flex-col flex-wrap md:flex-row justify-center items-center text-center my-8 py-8 overflow-x-hidden overflow-y-auto gap-4"
   >
-    <h1 class="text-lg lg:text-2xl text-neutral-400">No notes detected</h1>
+    <div
+      v-for="note in notes"
+      :key="note.id"
+      @click="note.expandNote = true"
+      class="w-80 h-80 my-4 text-xl bg-white lg:text-2xl text-neutral-900 font-medium cursor-pointer flex justify-center items-center shadow-xl rounded-md hover:scale-105 hover:bg-neutral-900 hover:text-white duration-300"
+    >
+      <h1>
+        {{ note.title }}
+      </h1>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { inject } from "vue";
+
+const notes = inject("notes");
+</script>
